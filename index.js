57edab63a906ce714c083d00ff3e1144c8bf8ba4
henriquewelbe <henriquewelbe@gmail.com>
2022-03-05 15:18:33 -0300

    Add EJS for templating the cards

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const ejs = require('ejs');
+const _ = require('lodash');
+const cars = require(__dirname + '/car-data.js');
+
+const app = express();
+app.set('view engine', 'ejs'); 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.locals._ = _;
+
+
+app.get('/', function(req, res){
+    res.render('index', {
+        cars: cars.cars(),
+    })
+})
+
+
+app.listen(3000, function(){
+    console.log('Server running on port 3000');
+})
