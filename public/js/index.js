53a96d0e432e1566b41b3fd4f0c9caa0633af605
henriquewelbe <henriquewelbe@gmail.com>
2022-03-07 15:43:11 -0300

    Add filter to select menu

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+// $('.option').on('click', function(){
+//     $('.type').each(function(item){
+//         console.log(item)
+//     })
+// })
+
+$('select').on('change', function () {
+    let optionText = $(this).find('option').filter(':selected').text();
+    $('.card').each(function(){
+        if(optionText === 'Vehicle Type'){
+            $('.card').show();
+        }else if ($(this).find('.type').text() !== optionText){
+            $(this).hide();
+        }else{
+            $(this).show();
+        }
+    })
+});
