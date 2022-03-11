eb328e8fa2c30d7abcbdc28866c102851381032a
henriquewelbe <henriquewelbe@gmail.com>
2022-03-10 22:25:45 -0300

    Add database for cars with Mongoose

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const ejs = require('ejs');
 const _ = require('lodash');
-const cars = require(__dirname + '/car-data.js');
+const mongoose = require('mongoose');
+const carsDB = require(__dirname + '/car-schema.js');
 
 const app = express();
 app.set('view engine', 'ejs'); 
 app.use(express.static('public'));
 app.locals._ = _;
 
+mongoose.connect('mongodb://localhost:27017/cars');
 
 app.get('/', function(req, res){
-    res.render('index', {
-        cars: cars.cars(),
+    carsDB.Car().find({}, function(err, results){
+        if (err){
+            console.log(err);
+        }else{
+            const cars = []
+            results.forEach(function(result){
+                cars.push(result);
+            })
+            res.render('index', {
+                cars: cars,
+            })
+        }
     })
 })
 
